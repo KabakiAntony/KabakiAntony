aa938aa457044b3ea5daa8bfdf3e99665feb4e24
Kabaki Antony <kabaki.antony@gmail.com>
2024-05-22 20:30:56 +0300

    create the urls for the me app

--- me/urls.py
+++ me/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import home, projects, blogs
 
+app_name = "me"
+
 urlpatterns = [
-    path('', home, name="me"),
+    path('', home, name="home"),
     path('projects/', projects, name="projects"),
     path('blogs/', blogs, name="blogs"),
 ]
