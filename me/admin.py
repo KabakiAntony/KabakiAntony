8a38b1e27634f412ac280f194b03878c82daf1bd
Kabaki Antony <kabaki.antony@gmail.com>
2024-05-23 10:26:43 +0300

    enable adminsite

--- me/admin.py
+++ me/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from kabakiantony.admin import ka_admin_site
+from .models import Me, Projects, Blogs
 
-# Register your models here.
+
+@admin.register(Me, site=ka_admin_site)
+class MeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', "thumb", "url"]
+    list_display_links = ['title']
+    list_filter = ['title']
+    
+
+@admin.register(Projects, site=ka_admin_site)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', "thumb", "url"]
+    list_display_links = ['title']
+    list_filter = ['title']
+
+
+@admin.register(Blogs, site=ka_admin_site)
+class BlogsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', "thumb", "url"]
+    list_display_links = ['title']
+    list_filter = ['title']
+
+
