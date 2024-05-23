c5dabd57f1ac247db8e91b05976b9173a0c942e7
Kabaki Antony <kabaki.antony@gmail.com>
2024-05-23 10:34:35 +0300

    add media url configuration

--- kabakiantony/urls.py
+++ kabakiantony/urls.py
@@ -1,8 +1,12 @@
 from .admin import ka_admin_site
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', ka_admin_site.urls),
     path('', include('me.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
